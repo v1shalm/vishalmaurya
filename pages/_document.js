20d31b36dd2844bb21e7abf36fc13fb01a0baccf
v1shalm <mauryav518@gmail.com>
2021-12-12 02:37:23 +0530

    added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,21 @@ class MyDocument extends Document {
          <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"/>
+        <meta name="description" content="I am a creative frontend developer, writing code and moving pixels in the www. I design and build custom web experiences for growing brands." />
+        <meta itemprop="name" content="V1shal Maurya - Frontend Developer" />
+         <meta itemprop="description" content="I am a creative frontend developer, writing code and moving pixels in the www. I design and build custom web experiences for growing brands." />
+          <meta itemprop="image" content="https://res.cloudinary.com/v1shalm/image/upload/v1639256324/portfolio_oq2y2n.png" />
+         <meta property="og:url" content="https://v1shal.vercel.app"/>
+          <meta property="og:type" content="website"/>
+            <meta property="og:title" content="V1shal Maurya - Frontend Developer"/>
+          <meta property="og:description" content="I am a creative frontend developer, writing code and moving pixels in the www. I design and build custom web experiences for growing brands." />
+           <meta property="og:image" content="https://res.cloudinary.com/v1shalm/image/upload/v1639256324/portfolio_oq2y2n.png" />
+
+            <meta name="twitter:card" content="summary_large_image"/>
+             <meta name="twitter:title" content="V1shal Maurya - Frontend Developer"/>
+            <meta name="twitter:description" content="I am a creative frontend developer, writing code and moving pixels in the www. I design and build custom web experiences for growing brands." />
+             <meta name="twitter:image" content="https://res.cloudinary.com/v1shalm/image/upload/v1639256324/portfolio_oq2y2n.png"/>
+
        </Head>
         <body>
           <Main />
